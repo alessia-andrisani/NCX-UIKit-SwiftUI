a38ee24770589cf16edb93969cd659ad935aa3e8
alessia-andrisani <alessiaandrisani@virgilio.it>
2022-04-04 15:53:53 +0200

    Changed date format style to medium.

--- Notes/Extensions.swift
+++ Notes/Extensions.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Date {
 	var formattedDateInString: String {
 		let dateFormatter = DateFormatter()
-		dateFormatter.dateStyle = .short
+		dateFormatter.dateStyle = .medium
 		return dateFormatter.string(from: self as Date)
 	}
 }
