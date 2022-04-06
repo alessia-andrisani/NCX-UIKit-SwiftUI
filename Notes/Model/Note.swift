a17f7f3928e4b85a7e999e9b56af3dfb7b0c72e5
alessia-andrisani <alessiaandrisani@virgilio.it>
2022-04-06 11:04:18 +0200

    Added more comments

--- Notes/Model/Note.swift
+++ Notes/Model/Note.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
+// Data modelling for a Note Object
 struct Note: Identifiable {
 	let id = UUID()
 	var title: String
 	var text: String
 	var date: Date
 	
+	// Example Note for the previews
 	static let exampleNote = Note(title: "Example Note", text: "This is an example note for the preview", date: .now)
 }
--- Notes/Model/NotesStore.swift
+++ Notes/Model/NotesStore.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// Class where we create an empty array of Notes. It needs to be ObservableObject in order to share it in the other views as a StateObject. The array needs to be Published in order to update the views when changes occur
+
 class NotesStore: ObservableObject {
 	@Published var notes: [Note] = []
 }
